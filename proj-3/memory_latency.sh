193cc900207640969974bbc3914aaddd54084c1c
Ryan Walton <ryan.walton.256@gmail.com>
2022-03-14 15:51:51 -0400

    Add project 3

---
+++ proj-3/memory_latency.sh
@@ -0,0 +1,11 @@
+./mlc --loaded_latency -l256 -R
+./mlc --loaded_latency -l256 -W12
+./mlc --loaded_latency -l256 -W3
+./mlc --loaded_latency -l256 -W2
+./mlc --loaded_latency -l256 -W5
+
+./mlc --loaded_latency -l64 -R
+./mlc --loaded_latency -l64 -W12
+./mlc --loaded_latency -l64 -W3
+./mlc --loaded_latency -l64 -W2
+./mlc --loaded_latency -l64 -W5
